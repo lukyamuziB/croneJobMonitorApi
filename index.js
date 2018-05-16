d0be11370466ca68cdde7a660a3c37887ec11f22
lukyamuziB <lukyamuzibenon@gmail.com>
2018-05-16 16:09:33 +0300

    ft add request parsing given a request path, trim it and console the trimed path without any extra '/'

--- index.js
+++ index.js
@@ -5,10 +5,25 @@
 
 //Dependencies
 const http = require('http');
+const url = require('url');
+
 //the server shoild respond to all requests with a string and listen on port 3000
 
 const server = http.createServer(function(req,res){
-    res.end('Hello there\n');
+    //get the URL and parse it
+    const parseUrl = url.parse(req.url, true);
+
+    //get the path
+    const path = parseUrl.pathname; 
+    const trimmedPath = path.replace(/^\/+|\+$/g, '');
+
+
+    //send the response
+    res.end('Hello there\n')
+
+    //log path
+    console.log('Request was recieved on: '+trimmedPath);
+
 })
 
 server.listen(3000, function(){
